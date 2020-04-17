3d1b3543b7618de76fbf1f21a68657af94b6b111
DarkCrusade <DarkCrusade@DESKTOP-QUJ2BAQ>
2020-04-17 21:18:39 +1000

    места работают, все считается, событие не навесил

--- Airlines/Scripts/PlaneSeatsEconom.js
+++ Airlines/Scripts/PlaneSeatsEconom.js
@@ -2,7 +2,9 @@
 // планов может быть и больше... и разные...
 k = 1;
 kb = 1;
+tbk = 0;
 counter = 0;
+$('#btn').attr("disabled", true);
 var PlaneHalle = {
     row: [6, 6, 6, 6, 6, 6, 6, 6]
 },
@@ -11,12 +13,12 @@ $.each(PlaneHalle.row, function (row, numberOfSeats) {
     PlaneHallRowe = '';
     for (i = 1; i <= numberOfSeats/2; i++) {
         // собираем ряды
-        PlaneHallRowe += '<div class="Eseat" id="' + 'a'+ k + '/' + i + '/' + 'e' + '"></div>';
+        PlaneHallRowe += '<div class="Eseat" id="'+ k + '/' + i + '/' + 'e' + '"></div>';
     }
     PlaneHallRowe += '<div class="emptyBtwSeatsEconom"></div>';
     for (i = numberOfSeats / 2 +1; i <= numberOfSeats; i++) {
         // собираем ряды
-        PlaneHallRowe += '<div class="Eseat" id="' + 'a' + k + '/' + i + '/' + 'e' + '"></div>';
+        PlaneHallRowe += '<div class="Eseat" id="'+ k + '/' + i + '/' + 'e' + '"></div>';
     }
     //собираем зал с проходами между рядами
     PlaneHallMape += PlaneHallRowe + '<div class="passageBetween">&nbsp;</div>';
@@ -31,17 +33,17 @@ $.each(PlaneHallb.row, function (row, numberOfSeats) {
     PlaneHallRowb = '';
     for (i = 1; i <= numberOfSeats / 2; i++) {
         // собираем ряды
-        PlaneHallRowb += '<div class="Bseat" id="' + 'a' + kb + '/' + i + '/' + 'b' + '"></div>';
+        PlaneHallRowb += '<div class="Bseat" id="'+ kb + '/' + i + '/' + 'b' + '"></div>';
     }
     PlaneHallRowb += '<div class="emptyBtwSeatsBuisness"></div>';
     for (i = numberOfSeats / 2 + 1; i <= numberOfSeats; i++) {
         // собираем ряды
-        PlaneHallRowb += '<div class="Bseat" id="' + 'a' + kb + '/' + i + '/' + 'b' + '"></div>';
+        PlaneHallRowb += '<div class="Bseat" id="'+ kb + '/' + i + '/' + 'b' + '"></div>';
     }
     PlaneHallRowb += '<div class="emptyBuisness"></div>';
     //собираем отсек с проходами между рядами
     PlaneHallMapb += PlaneHallRowb + '<div class="passageBetween">&nbsp;</div>';
-    k++;
+    kb++;
 });
 
 //заполняем в html зал номер 1
@@ -50,6 +52,7 @@ $('.buisness').html(PlaneHallMapb);
 // тут по клику определяем что место выкуплено
 $('.Eseat').on('click', function (e) {
     var counter = $('#c').val();
+    var id = e.currentTarget.id;
     // если первый раз кликнули билет выкупили, 
     // если повторно значит вернули билет
     if (e.currentTarget.classList.contains('no') == true) { }
@@ -59,13 +62,33 @@ $('.Eseat').on('click', function (e) {
             //показываем сколько билетов выкуплено
             counter++;
             $('#c').val(counter);
+            for (i = 0; i < $('#p').val(); i++) {
+                if ($('#seats_' + i + '_').val() == "") {
+                    $('#seats_' + i + '_').val(id);
+                    break;
+                }
+            }
+            if (counter < $('#p').val()) {
+                $('#btn').attr("disabled", true);
+            }
+            else {
+                $('#btn').attr("disabled", false);
+            }
         }
         else {
             if (e.currentTarget.classList.contains('bay')) {
+                var empty = "";
                 $(e.currentTarget).toggleClass('bay', false);
                 //показываем сколько билетов выкуплено
                 counter--;
                 $('#c').val(counter);
+                for (i = 0; i < $('#p').val(); i++) {
+                    if ($('#seats_' + i + '_').val() == id) {
+                        $('#seats_' + i + '_').val(empty);
+                        break;
+                    }
+                }
+                $('#btn').attr("disabled", true);
             }
         }
     }
@@ -73,8 +96,7 @@ $('.Eseat').on('click', function (e) {
 
 $('.Bseat').on('click', function (e) {
     var counter = $('#c').val();
-    // fl полуячаем из e e.currentTarget.GetAttr('а');
-    //
+    var id = e.currentTarget.id;
     // если первый раз кликнули билет выкупили, 
     // если повторно значит вернули билет
     if (e.currentTarget.classList.contains('no') == true) { }
@@ -84,13 +106,33 @@ $('.Bseat').on('click', function (e) {
             //показываем сколько билетов выкуплено
             counter++;
             $('#c').val(counter);
+            for (i = 0; i < $('#p').val(); i++) {
+                if ($('#seats_' + i + '_').val() == "") {
+                    $('#seats_' + i + '_').val(id);
+                    break;
+                }
+            }
+            if (counter < $('#p').val()) {
+                $('#btn').attr("disabled", true);
+            }
+            else {
+                $('#btn').attr("disabled", false);
+            }
         }
         else {
             if (e.currentTarget.classList.contains('bay')) {
+                var empty = "";
                 $(e.currentTarget).toggleClass('bay', false);
                 //показываем сколько билетов выкуплено
                 counter--;
                 $('#c').val(counter);
+                for (i = 0; i < $('#p').val(); i++) {
+                    if ($('#seats_' + i + '_').val() == id) {
+                        $('#seats_' + i + '_').val(empty);
+                        break;
+                    }
+                }
+                $('#btn').attr("disabled", true);
             }
         }
     }
@@ -98,5 +140,11 @@ $('.Bseat').on('click', function (e) {
 
 for (i = 0; i <= $('#ListSeats').length; i++) {
     var valu = document.getElementById("ListSeats").options[i].text;
-    document.getElementById("a"+valu).classList.add("no");
+    document.getElementById(valu).classList.add("no");
 }
+
+//$('#btn').onmouseover(function (e) {
+//    if (e.currentTarget.hasAttribute("disabled")) {
+//        alert('Выбрано недостаточно мест в самолете, пожалуйста, выберите недостующие места. Количество выбранных мест должно быть равным количеству пассажиров.');
+//    }
+//});
